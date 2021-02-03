4ec18e53b904f92e2b07a81e16c87f1aaf706667
mattyaak <to.akos@gmail.com>
2021-02-03 13:44:03 +0100

    ter

---
+++ wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿
+
+console.log("Hello World!");
+
+var theForm = document.getElementById("theForm")
+theForm.hidden = true;
+var button = document.getElementById("buyButton")
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+}
+);
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+listItems.hidden = true;
