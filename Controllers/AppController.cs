c64e34cd1789f4abd8713cb5c77bfaa974663be9
mattyaak <to.akos@gmail.com>
2021-02-03 13:44:46 +0100

    56

---
+++ Controllers/AppController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            throw new InvalidOperationException("Bad things happen");
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,15 +1,25 @@
-﻿
+﻿jQuery(document).ready(function () {
 
-console.log("Hello World!");
+    console.log("Hello World!");
 
-var theForm = document.getElementById("theForm")
-theForm.hidden = true;
-var button = document.getElementById("buyButton")
-button.addEventListener("click", function () {
-    console.log("Buying Item");
-}
-);
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItems = productInfo.item[0].children;
-listItems.hidden = true;
+    var theForm = jQuery("#theForm");
+    theForm.hide();
+    var button = jQuery("buyButton")
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = jQuery(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + jQuery(this).text());
+    });
+
+
+var $loginToggle = jQuery("#loginToggle");
+var $popupForm = jQuery(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(1000);
+    });
+});
